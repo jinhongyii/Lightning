78c7642bdad08a933db7d8b57e7b0cae02f92d4c
jinhongyi <3231950289@qq.com>
2020-04-27 17:54:13 +0800

    output assembly

--- src/backend/AsmPrinter.java
+++ src/backend/AsmPrinter.java
@@ -13,7 +13,7 @@ public class AsmPrinter implements Visitor {
     private void indent(){prefix+="\t";}
     private void dedent(){prefix=prefix.substring(0,prefix.length()-1);}
     private void print(String str){
-//        System.out.println(prefix+str);
+        System.err.println(prefix+str);
         try {
             bufferedWriter.write(prefix + str + "\n");
         } catch (IOException e) {
