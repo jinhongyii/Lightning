78c4138588491dfbaae91283992405267fc6b255
jinhongyi <3231950289@qq.com>
2020-05-19 09:21:36 +0800

    fix

--- src/backend/InstructionSelector.java
+++ src/backend/InstructionSelector.java
@@ -336,7 +336,7 @@ public class InstructionSelector implements IRVisitor {
                 if (offset == 0) {
                     curBB.addInst(new Move((Register)base,(Register)rd));
                 }
-                curBB.addInst(getTranslatedInst(Instruction.Opcode.add, new Imm(offset), base, (Register) rd));
+                curBB.addInst(getTranslatedInst(Instruction.Opcode.add, getOperand(new ConstantInt(offset),true), base, (Register) rd));
             }
         }
         return null;
